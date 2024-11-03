cc90fa92201f0d5e91c6f85a320e2f4a1e3e189c
ThatMingo <dom.norsic@hotmailc.com>
2024-11-03 12:56:03 +0100

    pray pray

--- boards/shields/eto/custom_status_screen.c
+++ boards/shields/eto/custom_status_screen.c
@@ -14,7 +14,7 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen = lv_obj_create(NULL);
 
     // Create a label for "Hello World"
-    lv_obj_t *label = lv_label_create(screen, NULL);
+    lv_obj_t *label = lv_label_create(screen); // Only pass the parent object
     lv_label_set_text(label, "Hello World");
     lv_obj_align(label, NULL, LV_ALIGN_CENTER, 0, 0); // Center the label
 
