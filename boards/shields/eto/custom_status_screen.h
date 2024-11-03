0142b4564c4b82952f27828d70be153dd8d4160f
ThatMingo <dom.norsic@hotmailc.com>
2024-11-03 14:28:58 +0100

    cat gpt answer ? I think its wronk

--- boards/shields/eto/custom_status_screen.h
+++ boards/shields/eto/custom_status_screen.h
@@ -1,5 +1,8 @@
-#pragma once
+#ifndef CUSTOM_STATUS_SCREEN_H
+#define CUSTOM_STATUS_SCREEN_H
 
-#include <lvgl.h>
+#include <lvgl/lvgl.h>
 
-//lv_obj_t *zmk_display_status_screen();
+lv_obj_t *zmk_display_status_screen();
+
+#endif // CUSTOM_STATUS_SCREEN_H
